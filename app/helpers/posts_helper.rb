1bde0388e055d043b221499b111ab2be81108e67
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-21 16:31:35 +0200

    enlace a los comments de cada post desde posts#index

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+
+  def link_to_comments(post)
+    count = post.comments.count
+    text  = "tiene #{count} comentario#{count == 1 ? '': 's'}"
+
+    link_to(text, post_comments_path(post))
+  end
+
 end
