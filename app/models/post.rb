4f6f4f414b5ac5087970a884c0e15bf4521f501c
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-21 08:51:31 +0200

    validaciones que pasan los tests del modelo de post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true, length: { minimum: 5 }
+
+  # validates(:url, {:format => { :with => Regexp.new('https?//.*') }})
+  validates :url, format: { with:  /https?:\/\/.*/ }
+
+  validates :description, length: { minimum: 10,
+                                    maximum: 255,
+                                    message: 'debe tener entre 10 y 255 caracteres'}
 end
