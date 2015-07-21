3e4c3e705e9aef4dc3730f32dff117859f25e515
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-21 16:36:27 +0200

    partial de formulario para comentarios en el index de comments errores en partial compartido

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,34 @@ class CommentsController < ApplicationController
 
   def index
     # Para probar que de llega a la accion con @post:
-    render text: @post.comments.map{|c| c.body }.join(',')
+    # render text: @post.comments.map{|c| c.body }.join(',')
 
     @comments = @post.comments # para la lista del each
+    @comment  = Comment.new    # para el form de nuevo comentario
   end
+
+  # def new
+  # # no lo tenemos, form en el index
+  # end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_comments_path(@post) , notice: 'Comment created!'
+    else
+      @comments = @post.comments
+      render :index
+    end
   end
 
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
