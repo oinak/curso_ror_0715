02233ec5c2913f167e53dca266c22c4e818c0cbc
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-21 08:48:28 +0200

    redirigir la ruta '/' al listado de posts (posts#index)

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#home'
+  # root 'pages#home'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
