72e7b9810dd161c4d9d418725f2951db8f5e99aa
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-21 08:49:29 +0200

    test y datos de prueba para posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "a new test" do
+    post = Post.new
+
+    assert !post.valid?, "a new post must not be valid"
+    assert_equal false, post.save
+    assert_raise(ActiveRecord::RecordInvalid){ post.save! }
+  end
+
+  test 'a post with no title' do
+    valid_post = posts(:caracola)
+
+    assert valid_post.valid?
+    valid_post.title = nil
+    assert_equal false, valid_post.valid?
+  end
+
 end
