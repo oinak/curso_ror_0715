0ea3120f55daf4837e9eafb083ac1f2854c78ce1
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-22 18:18:46 +0200

    primer ejercicio de scopes (solo modelo y consola)

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,18 @@ class Post < ActiveRecord::Base
   validates :description, length: { minimum: 10,
                                     maximum: 255,
                                     message: 'debe tener entre 10 y 255 caracteres'}
+
+  # scope :recent, lambda { |date = 1.week.ago|
+  #   where('created_at > ?', date)
+  # }
+  scope :recent, lambda { |*args|
+    # if args && args.first
+    #   date = args.first
+    # else
+    #   date = 1.week.ago
+    # end
+    date = ( args && args.first ? args.first : 1.week.ago )
+    where('created_at > ?', date)
+  }
+
 end
